656c6b780759386b03f703774865fb675cc3a24c
Jamal El Milahi <jamal@elmilahi.com>
2016-08-12 09:54:40 -0700

    Colorizing text

--- lib/ruby/alwan.rb
+++ lib/ruby/alwan.rb
@@ -1,7 +1,26 @@
 require 'ruby/alwan/version'
 
-module Ruby
-  module Alwan
-    # Your code goes here...
+class String
+  COLORS = {
+    nocolor:       "\e[0m",
+    black:         "\e[0;30;40m",
+    red:           "\e[0;31;40m",
+    green:         "\e[0;32;40m",
+    yellow:        "\e[0;33;40m",
+    purple:        "\e[0;34;40m",
+    lavender:      "\e[0;35;40m",
+    cyan:          "\e[0;36;40m",
+    white:         "\e[0;37;40m",
+    bold_red:      "\e[1;31;40m",
+    bold_green:    "\e[1;32;40m",
+    bold_yellow:   "\e[1;33;40m",
+    bold_purple:   "\e[1;34;40m",
+    bold_lavender: "\e[1;35;40m",
+    bold_cyan:     "\e[1;36;40m",
+    bold_white:    "\e[1;37;40m"
+  }
+
+  def colorize(color)
+    COLORS[color].to_s + self + COLORS[:nocolor]
   end
 end
--- spec/ruby/alwan_spec.rb
+++ spec/ruby/alwan_spec.rb
@@ -1,11 +1,22 @@
 require 'spec_helper'
 
-describe Ruby::Alwan do
-  it 'has a version number' do
-    expect(Ruby::Alwan::VERSION).not_to be nil
-  end
+describe String do
+  describe '#colorize' do
+    it 'returns the text colored' do
+      expect('foo bar'.colorize(:red)).to eq "\e[0;31;40mfoo bar\e[0m"
+    end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+    it 'returns section of the text colored' do
+      expect('foo'.colorize(:red) + ' bar').to eq "\e[0;31;40mfoo\e[0m bar"
+    end
+
+    it 'returns multi colored text' do
+      expect('b'.colorize(:red) + 'a'.colorize(:green) + 'r'.colorize(:yellow))
+        .to eq "\e[0;31;40mb\e[0m\e[0;32;40ma\e[0m\e[0;33;40mr\e[0m"
+    end
+
+    it 'returns text in the default color when the color is not found' do
+      expect('foo'.colorize(:bar)).to eq "foo\e[0m"
+    end
   end
 end
